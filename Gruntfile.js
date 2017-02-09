827b45339601a5c81a280558089c22b84da92efc
matthijs186 <matthijs186@live.nl>
2017-02-09 11:15:58 +0100

    Fix sass task, add watch sass task, update gitignore
    Also add newlines to all files

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,11 +8,21 @@ module.exports = function(grunt) {
 			},
 			dist: {
 				files: {
-					'style.css': 'style.scss'
+					'src/assets/css/style.css': 'src/assets/sass/style.scss'
 				}
+			}
+		},
+
+		watch: {
+			sass: {
+				files: ['src/assets/sass/**/*.scss'],
+				tasks: ['sass']
 			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['sass', 'watch']);
 };
